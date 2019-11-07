1c90c4303eb27a96ed86fc1ce803eeee30c67f3c
Your Name <keylung94@gmail.com>
2019-11-07 16:16:47 +0100

    removed component Scan

--- src/main/java/com/courseApi/course/config/JdbcConfig.java
+++ src/main/java/com/courseApi/course/config/JdbcConfig.java
@@ -1,7 +1,6 @@
 package com.courseApi.course.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -9,7 +8,6 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
 
 @Configuration
-@ComponentScan({"com.keySS"})
 public class JdbcConfig {
 
     @Bean
