0a5cffe9067e80bba2f2ce74dc3cdfe3fc4b8233
Your Name <keylung94@gmail.com>
2019-11-08 11:41:03 +0100

    removed authorization request to avoid CORS for the moment

--- src/main/java/com/courseApi/course/config/SpringSecurityConfig.java
+++ src/main/java/com/courseApi/course/config/SpringSecurityConfig.java
@@ -20,15 +20,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     // Secure the endpoins with HTTP Basic authentication
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-        http
-                //HTTP Basic authentication
-                .httpBasic()
-                .and()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/**").hasRole("USER")
-                .and()
-                .csrf().disable()
-                .formLogin().disable();
+                http.cors().and()
+                .httpBasic();
     }
 }
